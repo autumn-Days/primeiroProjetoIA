c4c873cbabaafcac9653cd1166acbb89ff7067db
autumnDays <117274518+autumn-Days@users.noreply.github.com>
2025-01-11 09:25:55 -0300

    Update finder.py

--- finder.py
+++ finder.py
@@ -1,6 +1,7 @@
 from typing import *
 import math
 import heapq
+from collections import deque
 
 class Finder:
     def __init__(self, origin:Tuple[int,int], destiny:Tuple[int, int], gridProportion=30) -> None:
@@ -34,14 +35,56 @@ class Finder:
             self.__showResult(totalCost,path)
         return (totalCost,path)
 
-    def __showResult(self,totalCost:int,path:List[Tuple[str,int,str]]) -> None:
-        print(f"Cost from {path[0][0]} to {self.destiny} : {totalCost}")
-        print(f"steps from {path[0][0]} to {self.destiny} : {len(path)}")
+    def BFS(self, costFunction:str, is2print=True):
+        totalCost,path = None,None
+    
+        if costFunction == 'C1':
+            totalCost,path = self.__BFS(self.C1)
+        elif costFunction == 'C2':
+            totalCost,path = self.__BFS(self.C2)
+        elif costFunction == 'C3':
+            totalCost,path = self.__BFS(self.C3)
+        else:
+            totalCost,path = self.__BFS(self.C4)
+        if is2print:
+            self.__showResult(totalCost,path)
+        return (totalCost,path)        
 
-        for i in range(len(path)):
-            print(f"{path[i][0]}--({path[i][1]})-->{path[i][2]}",end="")
-            if i != len(path)-1:
-                print(" --> ",end="")
+    def __BFS(self,costFun:Callable) -> Tuple[int,List[Tuple[str,int,str]]]:
+        """
+        É necessário guardar o pai do nó `current` para fazer o `traceback`,
+        a posição de cada nó gerado (obviamente) e o custo dele até o seu pai,
+        além da quantidade de passos que se leva do filho ao pai para que os 
+        custos C3 e C4 possam ser calculados.
+        
+        Portanto, é possível salvar essas informações em um dicionário com a 
+        estrutura:
+            
+        (x_{filho}, y_{filho}) : (custo, (x_{pai}, y_{pai}}), passos).
+        
+        Uma fila será usada para selecionar os nós que irão ser gerados, enquan-
+        to o dicionário guardará as informações necessárias para fazer o `trace
+        back`. Quanto a fila, era guardará elementos do tipo:
+            
+        (custo, (x_{filho, y_{filho}}), passos)
+        """
+        queue = deque()
+        queue.append((0,self.origin,0))
+                     
+        travelled = {self.origin: (0,str(self.origin)+"'",0)}
+        
+        while queue:
+            currentCost, currentNode, currentSteps = queue.popleft()
+            self.__nodesVisited.append(currentNode)
+            if currentNode == self.destiny:
+                return (currentCost, self.__pathTaken(travelled,self.destiny))
+            #It adds the generated nodes into the queue
+            neighbours = self.__getNeighbours(currentNode)
+            for neighbour in neighbours:
+                newCost = currentCost + costFun(self.__isItVertical(currentNode,neighbour), currentSteps+1)
+                queue.append((newCost,neighbour,currentSteps+1))
+                travelled[neighbour] = (newCost, currentNode, currentSteps+1)
+                
 
     def __uniformCost(self, costFunction:Callable) -> Tuple[int,List[Tuple[str,int,str]]]:
 #        In the way uniform cost search is implementend it depends upon a priority queue
@@ -69,43 +112,19 @@ class Finder:
         #stepsTaken = 0
         while priorityQueue:
             currentCost, currentNode, currentSteps = heapq.heappop(priorityQueue)
-            #parent = visited[currentNode][1]
-            #if currentNode != self.origin:
-            #    stepsTaken = visited[parent][2]+1
             if currentNode == self.destiny :
                 #The total cost is returned along with the way from the source to the destination
                 return (visited[self.destiny][0], self.__pathTaken(visited,self.destiny))
-            self.__nodesVisited.append(currentNode)#acho que o certo era gerado, dps eu mudo
+            self.__nodesVisited.append(currentNode)#acho que o certo era gerado, dps eu mudo. Tbm acho que poderia ser posto antes do if, dps vejo se dá certo
             #relaxamento
             neighbours = self.__getNeighbours(currentNode)
             for neighbour in neighbours:
                 totalCost = currentCost + costFunction(self.__isItVertical(currentNode, neighbour),currentSteps+1)
                 if ((neighbour not in visited) or (totalCost < visited[neighbour][0])):
-                    heapq.heappush(priorityQueue, (totalCost,neighbour,currentSteps+1   ))
+                    heapq.heappush(priorityQueue, (totalCost,neighbour,currentSteps+1))
                     visited[neighbour] = (totalCost,currentNode,currentSteps+1)
 
-    def __pathTaken(self, minSpanningTree:Dict[str,Tuple[int,str]], definitiveDestiny:str):
-        pathTaken:List[Tuple[str,int,str]] = []
-        
-        parent:str = minSpanningTree[definitiveDestiny][1]
-        changingDestiny:str = definitiveDestiny
 
-        while True:
-            pathTaken.append((minSpanningTree[changingDestiny][1],minSpanningTree[changingDestiny][0],changingDestiny))
-            changingDestiny = parent
-            parent = minSpanningTree[parent][1]
-            if parent == str(self.origin)+"'":
-                break
-        
-        pathTaken.reverse()
-        return pathTaken
-    def __getNeighbours(self,coord:Tuple[int,int]) -> List[Tuple[int,int]]:
-        neighboursBeta = [self.__goDown(coord), self.__goUp(coord), self.__goRight(coord), self.__goLeft(coord)]
-        #filter the invalid
-        for i in range(len(neighboursBeta)):
-            if (neighboursBeta[i][0] < 0 or neighboursBeta[i][0] > self.__gridProportion) or ((neighboursBeta[i][1] < 0 or neighboursBeta[i][1] > self.__gridProportion)):
-                neighboursBeta[i] = None
-        return [node for node in neighboursBeta if ((node != None) and (not self.__wasItVisited(node)))] # existe a possibilidade da lista retornada estar vazia
 
     def greedySearch(self, costFun, heuristicFun):#self, costFun:Callable[None], heuristicFun:Callable[Tuple[int,int]]):
         #somente se costFun.__name__ == C3 || == C4 então self.totalSteps setá incrementado
@@ -121,19 +140,6 @@ class Finder:
     def __goLeft(self, position) -> Tuple[int,int]:
             return (position[0]-1, position[1])
 
-    #methods related to heuristics
-    def euclidianHeuristic(self, neighbour:Tuple[int,int]) -> Tuple[int,int]:
-        return 10 * (self.__euc(neighbour, self.destiny))
-    def __euc(self,neighbour:Tuple[int,int], destiny:Tuple[int,int]):
-        x1,y1 = neighbour
-        x2,y2 = destiny
-        return math.floor(math.sqrt(pow(abs(x1-x2),2) + pow(abs(y1-y2),2)))
-    def manhatamHeuristic(self, neighbour:Tuple[int,int]):
-        return 10 * (self.__man(neighbour, self.destiny))
-    def __man(self,neighbour:Tuple[int,int], destiny:Tuple[int,int]):
-        x1,y1 = neighbour
-        x2,y2 = destiny
-        return abs(x1-x2) + abs(y1-y2)
     #methods related to cost calculations
     def C1(self, nothingImportant, nothingImportant2) -> None:
         return 10
@@ -150,22 +156,72 @@ class Finder:
         if isVertical:
             return 10
         return 5 + (abs((10-amountSteps%11)))
-    #other methods
-    def reset(self):
+
+    #methods related to heuristics
+    def euclidianHeuristic(self, neighbour:Tuple[int,int]) -> Tuple[int,int]:
+        return 10 * (self.__euc(neighbour, self.destiny))
+    def __euc(self,neighbour:Tuple[int,int], destiny:Tuple[int,int]):
+        x1,y1 = neighbour
+        x2,y2 = destiny
+        return math.floor(math.sqrt(pow(abs(x1-x2),2) + pow(abs(y1-y2),2)))
+    def manhatamHeuristic(self, neighbour:Tuple[int,int]):
+        return 10 * (self.__man(neighbour, self.destiny))
+    def __man(self,neighbour:Tuple[int,int], destiny:Tuple[int,int]):
+        x1,y1 = neighbour
+        x2,y2 = destiny
+        return abs(x1-x2) + abs(y1-y2)
+    
+    #auxiliary methods
+    def __showResult(self,totalCost:int,path:List[Tuple[str,int,str]]) -> None:
+        print(f"Cost from {path[0][0]} to {self.destiny} : {totalCost}")
+        print(f"steps from {path[0][0]} to {self.destiny} : {len(path)}")
+
+        for i in range(len(path)):
+            print(f"{path[i][0]}--({path[i][1]})-->{path[i][2]}",end="")
+            if i != len(path)-1:
+                print(" --> ",end="")
+
+    def __pathTaken(self, minSpanningTree:Dict[str,Tuple[int,str]], definitiveDestiny:str):
+        pathTaken:List[Tuple[str,int,str]] = []
+        
+        parent:str = minSpanningTree[definitiveDestiny][1]
+        changingDestiny:str = definitiveDestiny
+
+        while True:
+            pathTaken.append((minSpanningTree[changingDestiny][1],minSpanningTree[changingDestiny][0],changingDestiny))
+            changingDestiny = parent
+            parent = minSpanningTree[parent][1]
+            if parent == str(self.origin)+"'":
+                break
+        
+        pathTaken.reverse()
+        return pathTaken
+
+    def __getNeighbours(self,coord:Tuple[int,int]) -> List[Tuple[int,int]]:
+        neighboursBeta = [self.__goDown(coord), self.__goUp(coord), self.__goRight(coord), self.__goLeft(coord)]
+        #filter the invalid
+        for i in range(len(neighboursBeta)):
+            if (neighboursBeta[i][0] < 0 or neighboursBeta[i][0] > self.__gridProportion) or ((neighboursBeta[i][1] < 0 or neighboursBeta[i][1] > self.__gridProportion)):
+                neighboursBeta[i] = None
+        return [node for node in neighboursBeta if ((node != None) and (not self.__wasItVisited(node)))] # existe a possibilidade da lista retornada estar vazia
+
+    def __wasItVisited(self, coord:Tuple[int,int]):
+        return coord in self.__nodesVisited
+
+    def __isItVertical(self, currentNode:Tuple[int,int], neighbour:Tuple[int,int]):
+        return abs(currentNode[1] - neighbour[1]) > 0
+
+    def __reset(self):
         self.amountNodesGenerated:int = 0
         self.amountNodesVisited:int = 0
         #cost related atributes
         self.totalCost = 0
         self.totalSteps = 0
-    #auxiliary methods
-    def __wasItVisited(self, coord:Tuple[int,int]):
-        return coord in self.__nodesVisited
-    def __isItVertical(self, currentNode:Tuple[int,int], neighbour:Tuple[int,int]):
-        return abs(currentNode[1] - neighbour[1]) > 0
+
 def main():
-    a = Finder((0,0),(2,2),gridProportion=3)
+    a = Finder((0,0),(3,3),gridProportion=4)
     try:
-        b,c = a.uniformCost("C4")
+        b,c = a.BFS("C4")
         print(" ")
     except TypeError:
         print("deu ruim")
