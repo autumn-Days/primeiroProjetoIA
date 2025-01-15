d0a1b9ed6037908666cab0592df10be6b4b58f88
autumnDays <117274518+autumn-Days@users.noreply.github.com>
2025-01-15 03:47:08 -0300

    Update finder.py
    A ideia geral da geração dos casos que iremos analisar é essa. Não testei ainda, então espero que hajam pequenos erros de "compilação", mas bobos.

--- finder.py
+++ finder.py
@@ -9,13 +9,14 @@ from typing import *
 import math
 import heapq
 from collections import deque
+from random import randint
+import contextlib #serve para redirecionar o output do terminal para um arquivo
 
 class Finder:
-    def __init__(self, origin:Tuple[int,int], destiny:Tuple[int, int], gridProportion=30) -> None:
+    def __init__(self, origin:Tuple[int,int]=None, destiny:Tuple[int, int]=None, gridProportion=30) -> None:
         #atributes for the coordinates
         self.origin = origin
         self.destiny = destiny
-        self.currentPosition:Node = origin
         #atributes for statistics
         self.amountNodesGenerated:int = 0
         self.amountNodesVisited:int = 0
@@ -26,18 +27,22 @@ class Finder:
         self.__nodesVisited: List[Tuple[int,int]] = []
         self.__gridProportion = gridProportion
     #return (currentCost, self.__pathTaken(visited,self.destiny),pathCost,currentCost)
-    def runPathFiding(self, pathFidingAlgo:str, costFun:str, is2print=True):
+    def runPathFiding(self, pathFidingAlgo:str, costFun:str,heuristicFun:Callable=None, is2print=True):
         pathFidingAlgo:Callable = getattr(self,pathFidingAlgo)
         totalCost,path,pathCost,heuristicCost,isItHeuristic =  pathFidingAlgo(getattr(self,costFun,None), getattr(self,heuristicFun,None))
         if is2print:
             if not isItHeuristic:
                 self.__showResult(totalCost,path)
             else:
-                self.__showResult(totalCost,path,heuristicFun,pathCost,isItHeuristic)
+                self.__showResult(totalCost,path,isItHeuristic,pathCost,heuristicCost)
+            #elif pathFidingAlgo == "greedy":
+            #    self.__showResult(totalCost,path,isItHeuristic,pathCost,totalCost)
+            #elif pathFidingAlgo == "Astar":
+            #    self.__showResult(totalCost,path,isItHeuristic,pathCost,heuristicCost)
         return (totalCost,path)
     
     def __showResult(self,totalCost:int,path:List[Tuple[str,int,str]],isItHeuristic:str=None,pathCost:int=None,heuristicCost:int=None) -> None:
-        if heuristicType :
+        if isItHeuristic :
             print(f"Path cost from {path[0][0]} to {self.destiny}: {pathCost}")
             print(f"Heuristic cost from {path[0][0]} to {self.destiny}: {heuristicCost}")
         print(f"Total cost from {path[0][0]} to {self.destiny} : {totalCost}")
@@ -72,50 +77,63 @@ class Finder:
                             `(f(n),(x,y),passos,g(n))`
     """
 
-    def greedy(self, costFun:None, heuristic:Callable):
+    def greedy(self, costFun:Callable, heuristic:Callable):
         """
         Mesmo que o algoritmo guloso não utilize o custo do caminho, `costFun`
         é passado como parâmetro para facilitar a comparação entre ele e os
         algoritmos de busca não informados.
         """
-        heuristicValueOrigin = heuristic(self.origin)
+        IT_IS_HEURISTIC = True
         
-        queue = [(heuristicValueOrigin,self.origin,0,0)]
-        visited = {self.origin:(heuristicValueOrigin,str(self.origin)+"'",0,0)}
+        queue = [(0,self.origin,0,0)]
+        visited = {self.origin:(0,str(self.origin)+"'",0,0)}
 
         while queue:
             currentCost, currentNode, currentSteps,pathCost = heapq.heappop(queue)
             self.__nodesVisited.append(currentNode) #depois eu tenho que me lembrar de pôr isso antes da condição. Isso daqui explica pq eu não tive que subtrair 1 da quantidade de passos na função `pathTaken` para excluir a origem 
             if(currentNode == self.destiny):
-                return (currentCost, self.__pathTaken(visited,self.destiny),pathCost,currentCost,True)#f(n) = g(n), por isso currentCost é passado 2 vezes
+                #totalCost,path,pathCost,heuristicCost,isItHeuristic
+                return (currentCost, self.__pathTaken(visited,self.destiny),pathCost,currentCost,IT_IS_HEURISTIC)#f(n) = g(n), por isso currentCost é passado 2 vezes
             for neighbour in self.__getNeighbours(currentNode):
-                totalCost =  currentCost + heuristic(neighbour) #tem que somar com `currentCost`, se não o valor total será apresentado como "0" pq o custo heurístico do destino para o destino é zero
-                pathCostNeighbour = costFun(self.__isItVertical(currentNode, neighbour),currentSteps+1)
                 
+                if neighbour in self.__nodesGenerated:
+                    continue
+               
+                totalCost =  currentCost + heuristic(neighbour) #tem que somar com `currentCost`, se não o valor total será apresentado como "0" pq o custo heurístico do destino para o destino é zero
+                pathCostNeighbour = pathCost + costFun(self.__isItVertical(currentNode, neighbour),currentSteps+1)                
                 heapq.heappush(queue,(totalCost,neighbour,currentSteps+1,pathCostNeighbour))
                 visited[neighbour] = (totalCost,currentNode,currentSteps+1,pathCostNeighbour)
-
+                
+                self.__nodesGenerated.append(neighbour) #tem que adicionar a raiz
     def Astar(self, costFunction:Callable, heuristicFun:Callable) -> Tuple[int,List[Tuple[str,int,str]]]:
-        heuristicValueOrigin = heuristicFun(self.origin)
+        IT_IS_HEURISTIC = True
+        
 
-        priorityQueue = [(heuristicValueOrigin,self.origin,0)]#distance from parent, nextNode, steps taken to get from parent to nextNode
-        visited = {self.origin:(heuristicValueOrigin,str(self.origin)+"'",0)} #da fonte para a fonte, a distância é 0
+        priorityQueue = [(0,self.origin,0,0,0)]#distance from parent, nextNode, steps taken to get from parent to nextNode
+        visited = {self.origin:(0,str(self.origin)+"'",0,0,0)} #da fonte para a fonte, a distância é 0
         #stepsTaken = 0
         while priorityQueue:
-            currentCost, currentNode, currentSteps = heapq.heappop(priorityQueue)
+            currentCost, currentNode, currentSteps,pathCost,heuristicCost = heapq.heappop(priorityQueue)
             self.__nodesVisited.append(currentNode)#acho que o certo era gerado, dps eu mudo. Tbm acho que poderia ser posto antes do if, dps vejo se dá certo
             if currentNode == self.destiny :
                 #The total cost is returned along with the way from the source to the destination
-                return (visited[self.destiny][0], self.__pathTaken(visited,self.destiny))
+                #totalCost,path,pathCost,heuristicCost,isItHeuristic
+                return (visited[self.destiny][0], self.__pathTaken(visited,self.destiny),pathCost,heuristicCost,IT_IS_HEURISTIC)
             #relaxamento
             neighbours = self.__getNeighbours(currentNode)
             for neighbour in neighbours:
-                totalCost = currentCost + costFunction(self.__isItVertical(currentNode, neighbour),currentSteps+1) + heuristicFun(neighbour)
+                pathValue2neighbour = costFunction(self.__isItVertical(currentNode, neighbour),currentSteps+1)
+                heuristicValue2neighbour = heuristicFun(neighbour)
+                
+                totalCost = currentCost + pathValue2neighbour + heuristicValue2neighbour
+                newPathCost = pathCost + pathValue2neighbour
+                newHeuristicCost = heuristicCost + heuristicValue2neighbour
+                
                 if ((neighbour not in visited) or (totalCost < visited[neighbour][0])):
-                    heapq.heappush(priorityQueue, (totalCost,neighbour,currentSteps+1))
-                    visited[neighbour] = (totalCost,currentNode,currentSteps+1)
+                    heapq.heappush(priorityQueue, (totalCost,neighbour,currentSteps+1,newPathCost,newHeuristicCost))
+                    visited[neighbour] = (totalCost,currentNode,currentSteps+1,newPathCost,newHeuristicCost)
 
-    def BFS(self,costFun:Callable,heuristicFun=None) -> Tuple[int,List[Tuple[str,int,str]]]:
+    def D_BFS(self,costFun:Callable,popingMethod:str,heuristicFun=None) -> Tuple[int,List[Tuple[str,int,str]]]:
         """
         É necessário guardar o pai do nó `current` para fazer o `traceback`,
         a posição de cada nó gerado (obviamente) e o custo dele até o seu pai,
@@ -133,72 +151,35 @@ class Finder:
             
         (custo, (x_{filho, y_{filho}}), passos)
         """
+        
+        i = 0
+        
         queue = deque()
         queue.append((0,self.origin,0))
                      
-        travelled = {self.origin: (0,str(self.origin)+"'",0)}
-        
+        travelled = {self.origin: (0,str(self.origin)+"'",0)}       
         while queue:
-            currentCost, currentNode, currentSteps = queue.popleft()
+            if i%10 == 0:
+                print("oi")
+            currentCost, currentNode, currentSteps = getattr(queue,popingMethod) #usa `pop` para pilha e `popleft` para fila
+            
             self.__nodesVisited.append(currentNode)
             if currentNode == self.destiny:
                 return (currentCost, self.__pathTaken(travelled,self.destiny),None,None,None)
             #It adds the generated nodes into the queue
             neighbours = self.__getNeighbours(currentNode)
             for neighbour in neighbours:
+                #para que não haja sobreescrita de chaves à medida que elas são geradas
+                if neighbour in self.__nodesGenerated:
+                    continue
                 newCost = currentCost + costFun(self.__isItVertical(currentNode,neighbour), currentSteps+1)
                 queue.append((newCost,neighbour,currentSteps+1))
                 travelled[neighbour] = (newCost, currentNode, currentSteps+1)
-    
-    def DFS(self, costFunction:str, is2print=True):
-        totalCost, path = None, None
-
-        if costFunction == 'C1':
-            totalCost, path = self.__DFS(self.C1)
-        elif costFunction == 'C2':
-            totalCost, path = self.__DFS(self.C2)
-        elif costFunction == 'C3':
-            totalCost, path = self.__DFS(self.C3)
-        else:
-            totalCost, path = self.__DFS(self.C4)
-        if is2print:
-            self.__showResult(totalCost, path)
-        return (totalCost, path)
-
-    def __DFS(self, costFun: Callable) -> Tuple[int, List[Tuple[str, int, str]]]:
-
-        stack = [(0, self.origin, 0)]  # Pilha
-        travelled = {self.origin: (0, str(self.origin) + "'", 0)}
-        visited = set()
+                self.__nodesGenerated.append(neighbour)
+            i +=1
 
 
-        while stack:
-            currentCost, currentNode, currentSteps = stack.pop()
-
-            
-            if currentNode in visited:
-                continue
-            visited.add(currentNode)
-            self.__nodesVisited.append(currentNode)
-
-            self.__nodesVisited.append(currentNode)
-            
-            if currentNode == self.destiny:
-                print("destino encontrado")
-                return (currentCost, self.__pathTaken(travelled, self.destiny))
-            
-            neighbours = self.__getNeighbours(currentNode)
-            for neighbour in neighbours:
-                if neighbour not in visited:
-                    newCost = currentCost + costFun(
-                        self.__isItVertical(currentNode, neighbour), currentSteps + 1
-                    )
-                    stack.append((newCost, neighbour, currentSteps + 1))
-                    if neighbour not in travelled or newCost < travelled[neighbour][0]:
-                        travelled[neighbour] = (newCost, currentNode, currentSteps + 1)
-        
-        raise ValueError("caminho não encontrado!")
-        
+                
     def UCS(self, costFunction:Callable,heuristicFun=None) -> Tuple[int,List[Tuple[str,int,str]]]:
         """
         In the way uniform cost search is implementend it depends upon a priority queue
@@ -311,28 +292,126 @@ class Finder:
     def __isItVertical(self, currentNode:Tuple[int,int], neighbour:Tuple[int,int]):
         return abs(currentNode[1] - neighbour[1]) > 0
 
-    def __reset(self):
+"""
+        #atributes for the coordinates
+        self.origin = origin
+        self.destiny = destiny
+        #atributes for statistics
+        self.amountNodesGenerated:int = 0
+        self.amountNodesVisited:int = 0
+        #cost related atributes
+        self.totalCost = 0
+        #auxiliary atributs
+        self.__nodesGenerated:List[Tuple[int,int]] = []
+        self.__nodesVisited: List[Tuple[int,int]] = []
+        self.__gridProportion = gridProportion
+"""
+
+    def reset(self):
+        self.origin = None
+        self.destiny = None
         self.amountNodesGenerated:int = 0
         self.amountNodesVisited:int = 0
         #cost related atributes
         self.totalCost = 0
         self.totalSteps = 0
+        self.__nodesGenerated:List[Tuple[int,int]] = []
+        self.__nodesVisited: List[Tuple[int,int]] = []
+    
+class Test():
+    def generateData(self, is2GenerateNewCoordinates:bool, fileCoordinates=None):
+        randomCoordinates = []
+        if generateNewCoordinates:
+            randomCoordinates = self.__generateRandomCoordinates()
+        else:
+            randomCoordinates = self.__readCoordinateFile(fileCoordinate)
+        finder = Finder()
         
+        for coord in randomCoordinates:
+            x1,y1,x2,y2 = coord
+            finder.origin = (x1,y1)
+            finder.destiny = (x2,y2)
+            self.BFS_DFS_DATA(finder)
+            finder.reset()
+            self.UCS_DATA(finder)
+            finder.reset()
+            self.UCSData(finder)
+            finder.reset()
+            self.greedyData(finder)
+            finder.reset()
+    def AstarData(self, finder:Finder):
+        heuristics = ["euclidianHeuristic","manhatamHeuristic"]
+        costs = ["C1","C2","C3","C4"]
+
+        for heuristic in heuristics:
+            for cost in costs:
+                with open(f"Astar_{cost}_{heuristic}.txt","w") as file:    
+                    with contextlib.redirect_stdout(file):
+                        finder.runPathFiding("Astar",cost,heuristic)
+
+            
+    def BFS_DFS_Data(self,finder:Finder):
+        algos = ["BFS","DFS"]
+        costs = ["C1","C2","C3","C4"]
+
+        for algo in algos:
+            for cost in costs:
+                with open(f"{algo}_{cost}.txt","w") as file:
+                    with contextlib.redirect_stdout(file):
+                        if algo == "BFS":
+                            finder.runPathFiding(algo, cost,"popleft")
+                        else:
+                            finder.runPathFiding(algo, cost,"pop")
+        
+    def UCSData(self, finder:Finder):
+        costs = ["C1","C2","C3","C4"]
+        for cost in costs:
+            with open(f"{UCS}_{cost}.txt") as file:
+                with contextlib.redirect_stdout(file):
+                    finder.runPathFiding("UCS",cost,heuristic)    
+                
+    def greedyData(self, finder:Finder):
+        heuristics = ["euclidianHeuristic","manhatamHeuristic"]
+        costs = ["C1","C2","C3","C4"]
+
+        for heuristic in heuristics:
+            for cost in costs:
+                with open(f"{greedy}_{heuristic}_{cost}.txt") as file:
+                    with contextlib.redirect_stdout(file):                    
+                        finder.runPathFiding("greedy",cost,heuristic)
+
+    def __generateRandomCoordinates(self):
+        coordinates = []
+        for i in range(50):
+            
+            while True:
+                x1,y1,x2,y2 = (randint(0,30),randint(0,30),randint(0,30),randint(0,30))
+                if ((x1,y1) == (x2,y2) or (x1,y1,x2,y2) in coordinates):               
+                    continue
+                coordinates.append((x1,y1,x2,y2))
+                break
+        
+        with open("coordinates.txt","w") as file:
+            for coord in coordinates:
+                file.write(f"{coord[0]} {coord[1]} {coord[2]} {coord[3]}\n")
+        return coordinates
+
+
+    def __readCoordinateFile(self, file):
+        allCoordinates = []
+        with open(file,"r"):
+            for line in file:
+                if line != "\n":
+                    coordinates = tuple(map(int(line.strip().split()))) #coordinates = (x1,y1,x2,y2)
+                    allCoordinates.append(coordinates)
+        return allCoordinates
+
 def main():
-    a = Finder((0,0),(3,3),gridProportion=4)
-    x = Finder((0,0),(3,3),gridProportion=4)
+    a = Finder((0,0),(30,30),gridProportion=30)
     try:
-
-        b,c = a.runPathFiding("greedy","C","manhatamHeuristic")
+        b,c = a.runPathFiding("UCS","C1","manhatamHeuristic")
+        #b,c = a.BFS("C1")
         print(" ")
-
-
-        print("===================")
-        b, c = x.DFS("C1")
-        print(b)
-        print(c)
-        print("===================")
-        
     except TypeError:
         print("deu ruim")
     #a.tester()
@@ -340,12 +419,10 @@ if __name__ == "__main__":
     main()
 
 """
-casos de testes
-(0,0) -> (3,3) qProportion=3
-    Esse caso deu errado, pois existiam 1 caminho não adjacente com o mesmo peso
-    de um caminho adjacente, então, aconteceu de o caminho não adjacente ser es-
-    colhido.
+algoritmos que demoram muito do (0,0) para (8,8):
     
-    Portanto, antes de fazer o pop do dicionário, é necessário observar se o nódulo
-    que no qual o pop foi feito é adjacente ao currentNode.
+    a partir da 12° já demonstra falhas
+
+BFS
+greedy
 """
